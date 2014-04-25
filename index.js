4ac8da7b1bdf1c4a58b97aafa158e05fe4bf43a0
Grigor Yeghiazaryan <yeghiazaryan@gmail.com>
2014-04-26 00:29:05 +0400

    Pass JSON data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var app = express();
 var rest = require('restler');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser());
 
 var parseApiUrl = 'https://api.parse.com/1/';
 
@@ -15,20 +18,22 @@ app.get('/', function(req, res) {
 });
 
 app.post('/users', function(req, res) {
+  var user = req.body;
+  /*
   var user = {
     'username': 'skybase',
     'password': '5ky6453',
     'skybase': true
   };
+  */
   var userData = JSON.stringify(user);
-  console.log(userData);
   var url = parseApiUrl.concat('users');
   rest.post(url, {
     headers: {
       'X-Parse-Application-Id': process.env.APP_ID,
       'X-Parse-REST-API-Key': process.env.REST_API_KEY},
     data: userData
-    }).on('complete',function(data) {
+  }).on('complete',function(resp) {
       res.send('Done');
     });
 });
